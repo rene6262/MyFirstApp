ecb2e0f66b99c1ec962acebacdef0aae2e767462
rene6262 <rene6262@hotmail.com>
2019-09-25 13:42:26 -0400

    error

--- app/src/main/java/com/example/myfirstapp/User.java
+++ app/src/main/java/com/example/myfirstapp/User.java
@@ -1,5 +1,11 @@
 package com.example.myfirstapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import java.io.Serializable;
 
 public class User implements Serializable {
@@ -8,17 +14,17 @@ public class User implements Serializable {
     private String lastName;
     private boolean isAdmin;
 
-    public User (String firstName, String lastName, boolean isAdmin){
+    public User(String firstName, String lastName, boolean isAdmin) throws InvalidNameException {
         setFirstName(firstName);
         setLastName(lastName);
         setIsAdmin(isAdmin);
     }
 
-    public String getFirstName(){
+    public String getFirstName() {
         return firstName;
     }
 
-    public String getLastName(){
+    public String getLastName() {
         return lastName;
     }
 
@@ -26,16 +32,28 @@ public class User implements Serializable {
         return isAdmin;
     }
 
-    public void setFirstName(String newFirstName){
+    public void setFirstName(String newFirstName) throws InvalidNameException {
         firstName = newFirstName;
+        if(firstName.isEmpty()) {
+            throw new InvalidNameException("Name Could Not Be Submitted", new Throwable("Error"));
+            //Toast.makeText(this, InvalidNameException.getMessage(), Toast.LENGTH_LONG).show();
+        } else {
+            
+        }
     }
 
-    public void setLastName(String newLastName){
+    public void setLastName(String newLastName) throws InvalidNameException {
         lastName = newLastName;
+        if(lastName.isEmpty()) {
+            throw new InvalidNameException("Name Could Not Be Submitted", new Throwable("Error"));
+            //editFirst.requestFocus();
+            //editFirst.setError("Please Enter a Name");
+        }
     }
 
     public void setIsAdmin(boolean newIsAdmin) {
         isAdmin = newIsAdmin;
     }
-}
 
+
+}
